5319d859e2becd6791d64cf9e31bea4e310871e6
Emre Vatansever <vatanseveremre90@gmail.com>
2016-12-13 01:51:27 +0300

    message controller added

--- server/express/controller/message.js
+++ server/express/controller/message.js
@@ -1,5 +1,17 @@
 const message = {};
 
-module.exports  = {
+message.index = function (req,resp,next) {
+	
+};
+message.store = function (req,resp,next) {
+	
+};
+message.update = function (req,resp,next) {
+	
+};
+message.delete = function (req,resp,next) {
+	
+};
+module.exports = {
 	message
 }
