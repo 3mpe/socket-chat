0fc222f48108e0017ebe8303d26d89bbe5670c5f
Emre Vatansever <vatanseveremre90@gmail.com>
2016-12-13 01:51:55 +0300

    fallow route update

--- server/express/lib/route.js
+++ server/express/lib/route.js
@@ -1,22 +1,25 @@
-const { userCrud } = require('../controller/user');
-
 function load(app) {
-	/* crud operations */
+
+	const { userCrud } = require('../controller/user');
 	app.post('/user/login', userCrud.login);
 	app.post('/user/store', userCrud.store);
 	app.put('/user/update/:user_id', userCrud.update);
 	app.delete('/user/delete/:user_id', userCrud.delete);
 
-	const { fallow } = require('../controller/fallow');
+	const { fallow }  = require('../controller/fallow');
 	app.get('/user/:user_id/fallow', fallow.index);
-	app.post('/user/:user_id/fallow', fallow.store);
-	app.put('/user/:user_id/fallow', fallow.update);
-	app.delete('/user/:user_id/fallow', fallow.delete);
+	app.post('/user/:user_id/fallowing/:fallowing_id', fallow.storeFallowing);
+	app.post('/user/:user_id/fallowed/:fallowed_id', fallow.storeFallowed);
+	app.put('/user/:user_id/fallow/:fallowing_id', fallow.updateFallowing);
+	app.put('/user/:user_id/fallow/:fallowed_id', fallow.updateFallowed);
+	app.delete('/user/:user_id/fallow/:fallowing_id', fallow.deleteFallowing);
+	app.delete('/user/:user_id/fallow/:fallowed_id', fallow.deleteFallowed);
 
 	const { message } = require('../controller/message');
-	app.get('/user/:user_id/message',message.index);
-	app.delete('/user/:user_id/message',message.delete);
+	app.get('/user/:user_id/message', message.index);
+	app.delete('/user/:user_id/message', message.delete);
 }
-module.exports = { 
-	load 
+module.exports = {
+	load
 };
+
