93891ccd1e3ec1812c208973eda81007bbc5bb54
Emre Vatansever <vatanseveremre90@gmail.com>
2016-12-13 01:51:01 +0300

    fallow controller added

--- server/express/controller/fallow.js
+++ server/express/controller/fallow.js
@@ -1,5 +1,34 @@
+const User = require('../models/users');
+const User_fallow = require('../models/fallow');
 const fallow = {};
+
+fallow.index = function (req, resp, next) {
+	User.find({ _id: req.body.user_id }, function (error, User_data) {
+		if (error) { return resp.json({ message: error.errors }); }
+		return resp.json({ fallowed: User_data });
+	});
+};
+fallow.storeFallowing = function (req, resp, next) {
+	return resp.json({message: 'ok '});	
+};
+fallow.storeFallowed = function (req, resp, next) {
+	return resp.json({message: 'ok '});
+};
+fallow.updateFallowing = function (req, resp, next) {
+	return resp.json({message: 'ok '});
+};
+fallow.updateFallowed = function (req, resp, next) {
+	return resp.json({message: 'ok '});
+};
+
+fallow.deleteFallowing = function (req, resp, next) {
+	return resp.json({message: 'ok '});
+};
+fallow.deleteFallowed = function (req, resp, next) {
+	return resp.json({message: 'ok '});
+};
 
 module.exports = {
 	fallow
 }
+
